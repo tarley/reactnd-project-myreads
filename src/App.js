b9e601f17ccdce7f6c3961eca882607e5cf6a24e
Tarley Campos <tarley.campos@localiza.com>
2018-12-28 20:56:12 -0200

    style: Corrige a identacao do arquivo src/App.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,29 @@ import './App.css'
 
 
 class BooksApp extends React.Component {
-  state = {
-    a : {id: 1, nome: 2, teste: 3, x: 4, y: 5},
-    
-    bookshelves : [
-      {id: "currentlyReading", description: "Currently Reading"}, 
-      {id: "wantToRead", description: "Want to Read"}, 
-      {id: "read", description: "Read"}
-    ]
-  }
-  render() {
-    return (
-      <div className="app">
-        
-        <Route exact path='/' render={() => (
-          <BookShelves 
-              bookshelves={this.state.bookshelves}
-          />
-        )} />
-        
-        <Route path='/search' render={() => (
-          <SearchBooks />
-        )} />
-      </div>
-    )
-  }
+    state = {
+        bookshelves : [
+          {id: "currentlyReading", description: "Currently Reading"}, 
+          {id: "wantToRead", description: "Want to Read"}, 
+          {id: "read", description: "Read"}
+        ]
+    }
+    render() {
+        return (
+            <div className="app">
+
+            <Route exact path='/' render={() => (
+                <BookShelves 
+                    bookshelves={this.state.bookshelves}
+              />
+            )} />
+
+            <Route path='/search' render={() => (
+                <SearchBooks />
+            )} />
+          </div>
+        )
+    }
 }
 
 export default BooksApp
